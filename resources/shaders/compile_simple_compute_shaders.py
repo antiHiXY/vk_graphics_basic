a12726f901a3d678a351ad07a7dc7b1ff083fbaa
antiHiXY <fear_fa@mail.ru>
2021-10-21 23:02:13 +0300

    Half work of scan.

--- resources/shaders/compile_simple_compute_shaders.py
+++ resources/shaders/compile_simple_compute_shaders.py
@@ -5,8 +5,7 @@ import pathlib
 if __name__ == '__main__':
     glslang_cmd = "glslangValidator"
 
-    shader_list = ["simple.comp"]
+    shader_list = ["simple.comp", "dimple.comp"]
 
     for shader in shader_list:
         subprocess.run([glslang_cmd, "-V", shader, "-o", "{}.spv".format(shader)])
-
